9bfa1a1bcd6d17193d66832237a02ca7e3dae033
umer33 <umerhayat520@gmail.com>
2020-09-22 18:02:39 +0500

    updating

---
+++ src/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            <h3>username</h3>
+            {/* header -> avatar + username */}
+
+            {/* Image */}
+
+            {/* username + caption */}
+        </div>
+    )
+}
+
+export default Post
