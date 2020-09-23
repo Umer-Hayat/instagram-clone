50d47669dce8ff4392b991e19ac8be64a194e85a
umer33 <umerhayat520@gmail.com>
2020-09-23 17:52:13 +0500

    updating post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
+import { db } from "./firebase";
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+    // return () => {
+    //   cleanup
+    // }
+  }, [])
+
   return (
     <div className="app">
       <div className="app__header">
         <img
           className="app__headerImage"
-          src="images/logo.png"
+          src="/static/images/logo.png"
           alt="logo"
         />
       </div>
       {/* Header */}
 
-
+      {
+        posts.map(({ id, post }) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }
+      {/* 
       <Post />
+      <Post username="Hayat" caption="Wow React is very easy" imageUrl="/static/images/react.jpg" />
+      <Post username="Umer Hayat" caption="Wow Very nice" imageUrl="/static/images/react.jpg" /> */}
 
-      {/* Posts */}
+
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import './Post.css';
-import { Avatar } from '@material-ui/core/Avatar';
+import Avatar from '@material-ui/core/Avatar';
 
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
     return (
         <div className="post">
-            <h3>username</h3>
+            <div className="post__header">
+                <Avatar
+                    className="post__avatar"
+                    alt="Logo"
+                    src="/static/images/react.jpg"
+                />
+                <h3>{username}</h3>
+            </div>
             {/* header -> avatar + username */}
 
             <img className="post__image"
-                src="images/react.jpg" alt="react"
+                src={imageUrl} alt="react"
             />
             {/* Image */}
-            <h4 className="post__text"><strong>Umer Hayat</strong>: Wow very nice</h4>
+            <h4 className="post__text"><strong>{username}</strong>: {caption}</h4>
             {/* username + caption */}
         </div>
     )
