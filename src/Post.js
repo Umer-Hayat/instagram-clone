be0321ddefd9eea0a4b6e0d0b2ffbe5f3a50df17
umer33 <umerhayat520@gmail.com>
2020-09-23 07:50:48 +0500

    updating Post area

--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import './Post.css';
+import { Avatar } from '@material-ui/core/Avatar';
+
 
 function Post() {
     return (
-        <div>
+        <div className="post">
             <h3>username</h3>
             {/* header -> avatar + username */}
 
+            <img className="post__image"
+                src="images/react.jpg" alt="react"
+            />
             {/* Image */}
-
+            <h4 className="post__text"><strong>Umer Hayat</strong>: Wow very nice</h4>
             {/* username + caption */}
         </div>
     )
